72d2a055fa60a6d7aa4abf675f56e28e7b85787b
aliersh <ariel.diaz.borquez@icloud.com>
2024-12-19 17:20:48 -0500

    feat: add social link buttons
    - added SocialButtons component to the main page
    - updated Bio component styling
    - added react-icons and shadcn button dependencies

---
+++ src/components/SocialButtons.tsx
@@ -0,0 +1,49 @@
+import { Button } from "./ui/button";
+import { FaEnvelope, FaLinkedinIn, FaGithub } from "react-icons/fa6";
+import { GoArrowUpRight } from "react-icons/go";
+
+interface SocialLink {
+    href: string;
+    icon: React.ReactNode;
+    label: string;
+}
+
+const SocialButtons = () => {
+    const socialLinks: SocialLink[] = [
+        {
+            href: "mailto:ariel.diaz.borquez@icloud.com",
+            icon: <FaEnvelope />,
+            label: "Get in touch",
+        },
+        {
+            href: "https://github.com/aliersh",
+            icon: <FaGithub />,
+            label: "Check my code",
+        },
+        {
+            href: "https://www.linkedin.com/in/arielidiaz",
+            icon: <FaLinkedinIn />,
+            label: "Let's network",
+        },
+    ];
+
+    return (
+        <div className="flex gap-2">
+            {socialLinks.map((link) => (
+                <Button
+                    variant="outline"
+                    className="flex items-center gap-2 border-muted text-muted-foreground group"
+                    asChild
+                >
+                    <a href={link.href}>
+                        {link.icon}
+                        <span className="text-sm">{link.label}</span>
+                        <GoArrowUpRight className="transition-transform group-hover:rotate-45" />
+                    </a>
+                </Button>
+            ))}
+        </div>
+    );
+};
+
+export default SocialButtons;
